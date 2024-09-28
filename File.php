e00c8028137e726fc0ef3328f9f96466bcff0712
Mohammed-A-3 <csamohammed73@gmail.com>
2024-09-28 23:09:18 +0530

    File.php

--- File.php
+++ File.php
@@ -1,5 +1,5 @@
 <?php
-	date_default_timezone_set('UTC'); 
+	date_default_timezone_set('Asia/Kolkata'); 
 	$current_time = date('Y-m-d H:i:s');
 	echo "Hello Mohammed A, right now the time is $current_time";
 ?>
